ea4f70c43ff54e46afbe583ddda56e592ae7b128
Antal Matyas <antalmatyas9@gmail.com>
2020-06-23 11:32:52 +0300

    added static method usage example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Engine {
     String manufacturer;
     int capacity;
+
+    LocalDate manufacturingDate=LocalDate.now().minusWeeks(2);
 }
